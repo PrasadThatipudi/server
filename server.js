50f13d2527c9d35a33038ebf852dd8342218322d
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-03-19 15:18:46 +0530

    implements server for broadcasting

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
-const sendMessage = (sender, receiver, message) => {
-  console.log(message, sender, receiver);
+const sendMessage = (sender, receivers, message, encoder) => {
+  receivers.forEach((receiver) => {
+    const response = `${sender.clientName}: ${message}`;
+
+    receiver.write(encoder.encode(response));
+  });
+};
+
+const readClientName = async (client, decoder, encoder) => {
+  const requestMessage = "Please enter your name: ";
+
+  await client.write(encoder.encode(requestMessage));
+
+  const name = new Uint8Array(50);
+  const byteCount = await client.read(name);
+
+  return decoder.decode(name).slice(0, byteCount);
+};
+
+const closeConnection = (connection, encoder) => {
+  const exitMessage = "Connection closed successfully!\n";
+
+  connection.write(encoder.encode(exitMessage));
+  connection.close();
 };
 
 const handleConnection = async (connection, allConnections) => {
   const decoder = new TextDecoder();
   const encoder = new TextEncoder();
 
+  const clientName = await readClientName(connection, decoder, encoder);
+  connection.clientName = clientName.slice(0, -2);
+
   for await (const request of connection.readable) {
-    const message = decoder.decode(request).trim();
+    const message = decoder.decode(request);
 
-    if (message === "exit") {
-      const exitMessage = "Connection closed successfully!\n";
-
-      connection.write(encoder.encode(exitMessage));
-      connection.close();
+    if (message.trim() === "exit") {
+      closeConnection(connection, encoder);
       return;
     }
 
-    sendMessage(connection, allConnections.at(0), message);
+    sendMessage(connection, allConnections, message, encoder);
   }
 };
 
